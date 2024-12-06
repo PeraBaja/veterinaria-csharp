be02084c5781d5c3a455c49d8d4121e603d220a5
Benjamín Peralta <benjaminp158@gmail.com>
2024-12-06 14:54:47 -0300

    Completado Modelo Persona

--- Modelos/Persona.cs
+++ Modelos/Persona.cs
@@ -1,9 +1,10 @@
-using System.Net.Mail;
 
+using System.Text.RegularExpressions;
+using VeterinariaPractica.Utilidades;
 namespace VeterinariaPractica.Modelos {
     class Persona {
         string _nombre;
-        string _email;
+        public readonly string Email;
         string _telefono;
         string _direccion;
         public TipoTelefono TipoTelefono;
@@ -13,28 +14,34 @@ namespace VeterinariaPractica.Modelos {
         }
             _nombre = value;
         }}
-        
-        public string Email { get => _email; set {
-            try{
-                MailAddress mail = new MailAddress(value);
+
+        public Estado Estado { get; private set; }
+        string Telefono { get => _telefono; set {
+            string patron = @"^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{3}[-.\s]?\d{4,6}$";
+            if (!Regex.IsMatch(value, patron)) {
+                throw new ArgumentException("El telefono ingresado no es válido");
             }
-            catch {
+            _telefono = value;
+        }}
+        string Direccion { get => _direccion; set {
+              string patron = @"^\d+\s[A-z]+\s[A-z]+";
+              if (!Regex.IsMatch(value, patron)) {
+                throw new ArgumentException("El telefono ingresado no es válido");
+            }
+            _direccion = value;
+        }}
+        public Persona(string nombre, TipoTelefono tipoTelefono, string email, Estado estado, string telefono = "", string direccion = "") {
+            
+            if (!Validaciones.EsEmailValido(email)){
                 throw new ArgumentException("El mail ingresado no es válido.");
             }
-            finally {
-                _email = value;
-            }
-        }}
-        public Estado Estado { get; private set; }
-        string Telefono { get => _telefono; set {value}}
-        string Direccion { get => _direccion; set {value}}
-        public Persona(string nombre, TipoTelefono tipoTelefono, string email, Estado estado, string telefono = "", string direccion = "") {
+            Email = email;
             Nombre = nombre;
             TipoTelefono = tipoTelefono;
-            Email = email;
             Estado = estado;
             Telefono = telefono;
             Direccion = direccion;
         }
     }
+
 }
