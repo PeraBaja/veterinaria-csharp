5ef0ede4f54481b640ce793b9d6f09d887a404f4
Benjamín Peralta <benjaminp158@gmail.com>
2024-12-31 13:18:15 -0300

    Agregado funcionalidad a VistaVeterinario

--- Vistas/VistaVeterinario.cs
+++ Vistas/VistaVeterinario.cs
@@ -1,17 +1,32 @@
 using VeterinariaPractica.Modelos;
-
 namespace VeterinariaPractica.Vistas {
     class VistaVeterinario: IVistable<Veterinario> {        
-        public void Mostrar(string código) {
-
+        public void Mostrar(Veterinario veterinario) {
+                Console.WriteLine($"Número de matrícula: {veterinario.númeroMatricula}");
+                Console.WriteLine($"Nombre: {veterinario.Nombre}");
+                Console.WriteLine($"Email: {veterinario.email}");
+                Console.WriteLine($"Teléfono: {veterinario.Telefono}");
+                Console.WriteLine($"Dirección: {veterinario.Direccion}");
+                Console.WriteLine($"Email: {veterinario.email}");
         }
 
-        public void Listar() {
-
+        public void Listar(List<Veterinario> veterinarios) {
+            foreach (Veterinario veterinario in veterinarios){
+                Console.WriteLine("------------------------");
+                Mostrar(veterinario);
+                Console.WriteLine("------------------------");
+            }
         }
 
         public Veterinario PedirDatos() {
-            
+            Console.WriteLine("Ingrese el número de matrícula del veterinario");
+            string númeroMatricula = Console.ReadLine() ?? "";
+            Console.WriteLine("Ingrese el nombre del veterinario");
+            string nombre = Console.ReadLine() ?? "";
+            Console.WriteLine("Ingrese el email del veterinario");
+            string email = Console.ReadLine() ?? "";
+
+            return new Veterinario(númeroMatricula, nombre, email, new Estado());
         }
     }
 }
