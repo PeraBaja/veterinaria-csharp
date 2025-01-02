41060fe5cb8bca660fea843d62c9bd292129ef97
Benjamín Peralta <benjaminp158@gmail.com>
2025-01-02 18:53:38 -0300

    Probando controlador veterinario

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using VeterinariaPractica.Controladores;
 using VeterinariaPractica.Modelos;
+using VeterinariaPractica.Vistas;
 namespace VeterinariaPractica // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (!Directory.Exists("Archivos")) {
+                Directory.CreateDirectory("Archivos");
+            }
+            VistaVeterinario vistaVeterinario = new VistaVeterinario();
+            new ControladorVeterinario(vistaVeterinario).Listar();
         }
     }
 }
