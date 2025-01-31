04eabd7c1573f863f79f0a6156c68c8ec3fec234
Benjamín Peralta <benjaminp158@gmail.com>
2025-01-31 14:18:32 -0300

    Agregado modelo de Ficha Médica

--- Modelos/FichaMédica.cs
+++ Modelos/FichaMédica.cs
@@ -1,3 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VeterinariaPractica.Modelos {
-    
+    class FichaMédica {
+        [Required(ErrorMessage="Se necesita un código")]
+        public string Código { get; set;} = string.Empty;
+        
+        public List<string> CódigosConsultas { get; set;} = [];
+        [Required(ErrorMessage ="Se necesita un mail")]
+        [EmailAddress(ErrorMessage ="El mail ingresado no es válido")]
+        public string EmailVeterinario { get; set;} = string.Empty;  
+        
+    }
+
 }
